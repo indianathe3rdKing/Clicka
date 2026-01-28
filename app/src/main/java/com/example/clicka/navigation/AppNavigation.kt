a2d34eef4fe39871adfb326a59dc6f45f6356d61
indianathe3rd <indiemenziwa@gmail.com>
2026-01-28 22:52:36 +0200

    Define App Navigation

--- app/src/main/java/com/example/clicka/navigation/AppNavigation.kt
+++ app/src/main/java/com/example/clicka/navigation/AppNavigation.kt
@@ -1,16 +1,30 @@
 package com.example.clicka.navigation
 
 import android.os.Bundle
+import android.view.RoundedCorner
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.navigator.tab.CurrentTab
+import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabNavigator
 import com.example.clicka.ui.theme.ClickaTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,9 +33,52 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ClickaTheme {
+                TabNavigator(SelectTab) {
+                    Scaffold(
+                        content = { innerPadding ->
+                            Box(modifier = Modifier.padding(innerPadding)) {
+                                CurrentTab()
+                            }
+                        },
 
+                        bottomBar = {
+                            Surface(
+                                shape = RoundedCornerShape(24.dp)
+                            ) {
+                                NavigationBar() {
+                                    TabNavigationItem(SelectTab)
+                                    TabNavigationItem(SettingsTab)
+                                }
+                            }
+                        }
+                    )
+                }
             }
         }
     }
 }
 
+@Composable
+private fun RowScope.TabNavigationItem(tab: Tab) {
+    val tabNavigator = LocalTabNavigator.current
+    val isSelected = tabNavigator.current == tab
+
+    NavigationBarItem(
+        selected = tabNavigator.current == tab,
+        onClick = { tabNavigator.current = tab },
+        icon = {
+            Icon(
+                painter = tab.options.icon!!, contentDescription = tab.options.title,
+                tint = if (isSelected) MaterialTheme.colorScheme.primary
+                else MaterialTheme.colorScheme.inversePrimary
+            )
+        },
+        label = {
+            Text(
+                tab.options.title,
+                color = if (isSelected) MaterialTheme.colorScheme.onSurface
+                else MaterialTheme.colorScheme.inverseOnSurface
+            )
+        }
+    )
+}
