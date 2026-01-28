b8193f6d3f154442e774e8a4ddd3a85f1cca6a3c
indianathe3rd <indiemenziwa@gmail.com>
2026-01-28 23:05:27 +0200

    SelectTab nav Boilerplate

--- app/src/main/java/com/example/clicka/screens/select/SelectTab.kt
+++ app/src/main/java/com/example/clicka/screens/select/SelectTab.kt
@@ -1,2 +1,41 @@
 package com.example.clicka.screens.select
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import cafe.adriel.voyager.navigator.tab.Tab
+import cafe.adriel.voyager.navigator.tab.TabOptions
+
+object SelectTab: Tab{
+    override val options: TabOptions
+        @Composable
+        get(){
+            val title = "Select"
+            val icon = rememberVectorPainter(Icons.Default.Create)
+
+            return remember {
+                TabOptions(
+                    index=0u,
+                    title=title,
+                    icon=icon
+                )
+            }
+        }
+
+    @Composable
+    override fun Content() {
+        TODO("Not yet implemented")
+        Box(modifier= Modifier.fillMaxSize()) {
+            Text(text = "Select Tab", modifier = Modifier.align(Alignment.Center))
+        }
+    }
+
+
+}
