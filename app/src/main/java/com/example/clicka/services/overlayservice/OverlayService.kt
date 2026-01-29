9d9e4ba94861ef97232a6967c617d5ac3b917565
indianathe3rd <indiemenziwa@gmail.com>
2026-01-29 17:18:04 +0200

    feat: overlay FAB

--- app/src/main/java/com/example/clicka/services/overlayservice/OverlayService.kt
+++ app/src/main/java/com/example/clicka/services/overlayservice/OverlayService.kt
@@ -8,7 +8,34 @@ import android.os.IBinder
 import android.provider.Settings
 import android.view.Gravity
 import android.view.WindowManager
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Build
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelStore
 import androidx.lifecycle.ViewModelStoreOwner
@@ -16,8 +43,10 @@ import androidx.lifecycle.setViewTreeLifecycleOwner
 import androidx.lifecycle.setViewTreeViewModelStoreOwner
 import androidx.savedstate.setViewTreeSavedStateRegistryOwner
 import com.example.clicka.services.overlaylifecycleowner.OverlayLifecyeOwner
+import kotlin.math.roundToInt
 
 class OverlayService : Service() {
+    val lifecycleOwner = OverlayLifecyeOwner()
 
     private val windowManager
         get() = getSystemService(WINDOW_SERVICE) as WindowManager
@@ -25,6 +54,7 @@ class OverlayService : Service() {
 
     override fun onCreate() {
         super.onCreate()
+        showOverlay()
 
     }
 
@@ -62,7 +92,14 @@ class OverlayService : Service() {
         val composeView = ComposeView(this).apply { isClickable=true }
 
         composeView.setContent {
-//        TODO: Composable
+        FloatingButton(
+            onMoveBy = {
+                dragX,dragY->
+                params.y+=dragY
+                params.x+=dragX
+                windowManager.updateViewLayout(composeView,params)
+            },{stopSelf()}
+        )
         }
 
 
@@ -73,7 +110,6 @@ class OverlayService : Service() {
 
         }
 
-        val lifecycleOwner = OverlayLifecyeOwner()
 
         lifecycleOwner.performRestore(null)
         lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
@@ -86,6 +122,8 @@ class OverlayService : Service() {
     }
 
     override fun onDestroy() {
+        lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
         overlayView?.let{
             try {
                 windowManager.removeView((it))
@@ -99,5 +137,53 @@ class OverlayService : Service() {
     override fun onBind(intent: Intent?): IBinder? {
         return null
 
+    }
+}
+
+@Composable
+private fun FloatingButton(
+    onMoveBy:(dragX: Int,dragY: Int)-> Unit,
+    onClose:()-> Unit
+
+){
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .wrapContentHeight()
+            .wrapContentWidth()
+    ) {
+        AnimatedVisibility(
+            visible = expanded ,
+            enter= fadeIn()+ slideInVertically(initialOffsetY = {it})+ expandVertically(),
+            exit = fadeOut()+ slideOutVertically(targetOffsetY = {it})+ shrinkVertically()
+        ) {
+            FloatingActionButton(
+                onClick = {},
+                containerColor = Color(0,150,136,255),
+                modifier = Modifier.padding(6.dp)
+            ) {
+                Icon(Icons.Filled.Build,null)
+            }
+        }
+
+        FloatingActionButton(
+            onClick = {},
+            modifier = Modifier
+                .pointerInput(Unit){
+                    detectDragGestures { change, dragAmount ->
+                        change.consume()
+                        onMoveBy(
+                            dragAmount.x.roundToInt(),
+                            dragAmount.y.roundToInt()
+                        )
+                    }
+                }
+            ,containerColor = Color(0,150,136,255),
+            elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+        ) {
+            Icon(imageVector = Icons.Filled.Add,null)
+        }
     }
 }
