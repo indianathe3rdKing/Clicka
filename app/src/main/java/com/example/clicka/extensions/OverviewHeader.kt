64deecb6c140485fe10ed95c8fe7ae29c167d395
indianathe3rd <indiemenziwa@gmail.com>
2026-01-29 11:31:55 +0200

    OverviewHeader Component

---
+++ app/src/main/java/com/example/clicka/extensions/OverviewHeader.kt
@@ -0,0 +1,59 @@
+package com.example.clicka.extensions
+
+import android.icu.text.CaseMap
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+internal fun OverviewHeader(
+    title: String,
+
+) {
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+    ) {
+        Text(title, style = MaterialTheme.typography.headlineLarge,
+            color = MaterialTheme.colorScheme.onSurface,
+            modifier = Modifier.weight(1f)
+
+            )
+        FloatingActionButton(
+            onClick = {},
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+            .border(
+                1.dp,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.shapes.large
+
+            ),
+            containerColor = Color.Transparent
+        ) {
+            Icon(imageVector = Icons.Filled.Add,null, tint = MaterialTheme.colorScheme.onSurface)
+        }
+    }
+
+}
--- app/src/main/java/com/example/clicka/screens/select/SelectTab.kt
+++ app/src/main/java/com/example/clicka/screens/select/SelectTab.kt
@@ -2,6 +2,7 @@ package com.example.clicka.screens.select
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.Text
@@ -10,8 +11,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
+import com.example.clicka.extensions.OverviewHeader
 
 object SelectTab: Tab{
     override val options: TabOptions
@@ -31,9 +34,10 @@ object SelectTab: Tab{
 
     @Composable
     override fun Content() {
-        TODO("Not yet implemented")
-        Box(modifier= Modifier.fillMaxSize()) {
-            Text(text = "Select Tab", modifier = Modifier.align(Alignment.Center))
+
+        Box(modifier= Modifier.fillMaxSize()
+            .padding(20.dp,30.dp)) {
+            OverviewHeader("Clicka")
         }
     }
 
