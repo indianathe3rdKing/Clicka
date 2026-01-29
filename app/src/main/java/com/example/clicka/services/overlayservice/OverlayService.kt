898db3e8dc2c61af7ef42b20af82ef448de72d8d
indianathe3rd <indiemenziwa@gmail.com>
2026-01-29 16:46:06 +0200

    feat : Overylay service

--- app/src/main/java/com/example/clicka/services/overlayservice/OverlayService.kt
+++ app/src/main/java/com/example/clicka/services/overlayservice/OverlayService.kt
@@ -2,15 +2,99 @@ package com.example.clicka.services.overlayservice
 
 import android.app.Service
 import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
 import android.os.IBinder
+import android.provider.Settings
+import android.view.Gravity
+import android.view.WindowManager
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.lifecycle.setViewTreeViewModelStoreOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+import com.example.clicka.services.overlaylifecycleowner.OverlayLifecyeOwner
 
-class OverlayService: Service() {
+class OverlayService : Service() {
 
-    private val winw
+    private val windowManager
+        get() = getSystemService(WINDOW_SERVICE) as WindowManager
+    private var overlayView: ComposeView? = null
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+    private fun showOverlay() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && !Settings.canDrawOverlays(this)) {
+            stopSelf()
+            return
+        }
+
+        if (overlayView != null) return
+
+        val layoutFlag: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            @Suppress("DEPRECATION")
+            WindowManager.LayoutParams.TYPE_PHONE
+        }
 
 
+        val params = WindowManager.LayoutParams(
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            WindowManager.LayoutParams.WRAP_CONTENT,
+            layoutFlag,
+            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN or
+                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE or
+                    WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL,
+            PixelFormat.TRANSLUCENT
+
+        ).apply {
+            gravity = Gravity.TOP or Gravity.START
+            x = 0
+            y = 200
+        }
+
+        val composeView = ComposeView(this).apply { isClickable=true }
+
+        composeView.setContent {
+//        TODO: Composable
+        }
 
 
+        val viewModelStore = ViewModelStore()
+        val viewModelStoreOwner= object : ViewModelStoreOwner{
+            override val viewModelStore: ViewModelStore
+                get() = viewModelStore
+
+        }
+
+        val lifecycleOwner = OverlayLifecyeOwner()
+
+        lifecycleOwner.performRestore(null)
+        lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
+        lifecycleOwner.handleLifecycleEvent(Lifecycle.Event.ON_START)
+
+        composeView.setViewTreeLifecycleOwner(lifecycleOwner)
+        composeView.setViewTreeSavedStateRegistryOwner(lifecycleOwner)
+        composeView.setViewTreeViewModelStoreOwner(viewModelStoreOwner)
+
+    }
+
+    override fun onDestroy() {
+        overlayView?.let{
+            try {
+                windowManager.removeView((it))
+            }catch (_: Throwable){
+
+            }
+        }
+        super.onDestroy()
+    }
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
