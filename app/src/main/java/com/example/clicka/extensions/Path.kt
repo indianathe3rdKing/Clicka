91e6b470c6821803536b6a82103b30b4c8384c81
indianathe3rd <indiemenziwa@gmail.com>
2026-02-02 21:08:40 +0200

    implament safe click  path

---
+++ app/src/main/java/com/example/clicka/extensions/Path.kt
@@ -0,0 +1,11 @@
+package com.example.clicka.extensions
+
+import android.graphics.Path
+import kotlin.math.max
+
+fun Path.safeMoveTo(x: Int,y:Int): Unit =
+    moveTo(max(0,x).toFloat(),max(0,y).toFloat())
+
+fun Path.safeLineTo(x:Int,y:Int): Unit =
+    lineTo(max(0,x).toFloat(),max(0,y).toFloat())
+
