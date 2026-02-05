7eaf3d48e8984ce4b5cfcebab220e6d887da8399
indianathe3rd <indiemenziwa@gmail.com>
2026-02-05 03:38:54 +0200

    implement repeat suspend

--- app/src/main/java/com/example/clicka/engine/Repeat.kt
+++ app/src/main/java/com/example/clicka/engine/Repeat.kt
@@ -1,2 +1,26 @@
 package com.example.clicka.engine
 
+import com.example.clicka.domain.model.Repeatable
+import com.example.clicka.domain.model.RepeatableWithDelay
+import kotlinx.coroutines.delay
+
+
+internal suspend fun Repeatable.repeat(action: suspend()-> Unit): Unit =
+    when {
+        isRepeatIfinite-> while (true){
+            action()
+            delayNextActionIfNeeded()
+        }
+        repeatCount>0-> repeat(repeatCount){
+            action()
+            delayNextActionIfNeeded()
+        }
+        else -> Unit
+    }
+
+private suspend fun Repeatable.delayNextActionIfNeeded(){
+    if (this !is RepeatableWithDelay) return
+    if (repeatDelayMs ==0L) return
+
+    delay(repeatDelayMs)
+}
